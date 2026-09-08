8b05bb4b04accffa9ddb9317cf1f10198324faaa
agungadisaputra04 <agungadisaputra05@gmail.com>
2026-09-08 09:51:04 +0700

    Fix root endpoint test

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
 
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert response.json() == {
         "message": "API is running",
 	"version": "1.0"
